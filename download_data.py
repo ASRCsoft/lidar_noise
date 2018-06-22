d18eeca5181c819deb8778858dc075a6d2e267b9
William May <williamcmay@live.com>
2018-06-22 18:02:55 -0400

    Added to download_data.py

--- download_data.py
+++ download_data.py
@@ -2,3 +2,14 @@
 
 import sqlalchemy
 import xarray as xr
+
+# set up database connection
+with open('database.txt') as f:
+    dbconfig = f.readlines()[0].strip()
+pg = sqlalchemy.create_engine(dbconfig)
+
+# get data
+# ...
+
+# write to netcdf file
+# ...
